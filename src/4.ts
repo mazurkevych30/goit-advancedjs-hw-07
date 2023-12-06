775f1bafb7e079e1a806ac6b4ec630c7aefd377a
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2023-12-06 00:25:30 +0000

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+
+class Key {
+    private signature: number = Math.random();
+
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key){
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person):void {
+        if (!this.door) {
+            console.log("Door closed!");
+            return;
+        }
+
+        this.tenants.push(person);
+        this.door = false;
+    }
+
+
+    public abstract openDoor(key: Key): void;
+
+}
+
+class MyHouse extends House {
+
+    constructor(key:Key) {
+        super(key)
+    }
+
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +63,6 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+console.log(house);
 
 export {};
